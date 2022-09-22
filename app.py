cd6437d3f9d517f174e44fd0861d992dc853b423
EgorTarasov <tarasov.egor.yandex@gmail.com>
2022-09-22 11:26:00 +0300

    changed LibController

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+import sys
 from controller import LibController
+
+# TODO: Cli tool for downloading
+
+
+# https://docs.python.org/3/library/argparse.html
+
+
+def main(foo, bar, **kwargs):
+    print(foo, bar, kwargs)
+    pass
 
 
 if __name__ == "__main__":
-    l = LibController()
-    print("\n".join([f"{doc.id}, {doc.name}" for doc in l.find_doc("Мат")]))
-    doc_id = input("Введите id:")
-    l.download_doc(doc_id)
+    main(
+        sys.argv[1],  # foo
+        sys.argv[2],  # bar
+        **dict(arg.split("=") for arg in sys.argv[3:])
+    )
+    # l = LibController()
+    # print("\n".join([f"{doc.id}, {doc.name}" for doc in l.find_doc("Мат")]))
+    # doc_id = input("Введите id:")
+    # l.download_doc(doc_id)
--- controller.py
+++ controller.py
@@ -1,14 +1,11 @@
 import os
 import time
-import logging
-import json
-import requests
 from PIL import Image
 
-from models import Folder, Document
+import models as models
 from loader import Session
 
-
+from progress.bar import IncrementalBar
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -27,9 +24,28 @@ class LibController:
     document_url = "http://elibrary.misis.ru/action.php?kt_path_info=ktcore.SecViewPlugin.actions.document&fDocumentId={}"
     page_url = "http://elibrary.misis.ru/plugins/SecView/getDoc.php?id={}&page={}&type=small/{}"
     pages_path = "pages/"
+    image_name = "test_pages/{}.png"
 
-    def __init__(self) -> None:
+    def __init__(self, debug=False) -> None:
         self.db_session = None
+
+        # initializing selenium driver
+        options = Options()
+        if debug:
+            options.headless = False
+        else:
+            options.headless = True
+        options.add_argument("window-size=1920,1080")
+        self.__driver = webdriver.Chrome("drivers/chromedriver", options=options)
+
+        self.__auth_delay = 2
+
+        self.__auth()
+
+    def __del__(self):
+        print("deleted controller")
+        #self.__driver.quit()
+        #time.sleep(self.__auth_delay)
 
     def get_folder(self):
         """
@@ -37,170 +53,101 @@ class LibController:
         """
         pass
 
-    def get_doc(self, doc_id: int):
+    def __get_doc(self, doc_id: int):
         """
         Получение пути / документа
         """
         if self.db_session == None:
             self.db_session = Session()
-        d = self.db_session.query(Document).filter_by(id=doc_id).one()
+        d = self.db_session.query(models.Document).filter_by(id=doc_id).one()
         self.db_session = None
         return (doc_id, d.name)
 
-    def login(self):
-        delay = 1
-        user_id, name = 2104105, "Егор"
-        self.driver.get(AUTH_URL)
-        time.sleep(delay)
-        input_login = self.driver.find_element(By.NAME, "username")
-        input_login.send_keys(user_id)
-        input_password = self.driver.find_element(By.NAME, "password")
-        input_password.send_keys(name)
-        self.driver.find_element(
-            By.CSS_SELECTOR, "#formbox > form > div > input[type=submit]"
-        ).click()
-        time.sleep(delay)
+    def __auth(self, user_id: str = "2104105", name: str = "Егор"):
 
-    def download_doc(self, document_id: int):
-        # TODO: create progress bar
-        options = Options()
-        options.headless = True
-        options.add_argument("window-size=1920,1080")
-        driver = webdriver.Chrome("drivers/chromedriver", options=options)
-        delay = 2
-        user_id, name = 2104105, "Егор"
-
-        # Auth in eliblary
         auth = False
-        driver.get(AUTH_URL)
+        self.__driver.get(AUTH_URL)
         while not auth:
             try:
-                input_login = driver.find_element(By.NAME, "username")
+                input_login = self.__driver.find_element(By.NAME, "username")
             except NoSuchElementException:
                 continue
             input_login.send_keys(user_id)
-            input_password = driver.find_element(By.NAME, "password")
+            input_password = self.__driver.find_element(By.NAME, "password")
             input_password.send_keys(name)
-            submit_button = driver.find_element(
+            self.__driver.find_element(
                 By.CSS_SELECTOR, "#formbox > form > div > input[type=submit]"
             ).click()
-            time.sleep(delay)
+            time.sleep(self.__auth_delay)
             auth = True
 
-        # Auth in elibrary
-
-        # goto document page and download images
-        image_url = "http://elibrary.misis.ru/"
-        driver.get(DOCUMENT_URL.format(document_id))
+    def __get_page_count(self, document_id: int) -> int:
+        self.__driver.get(DOCUMENT_URL.format(document_id))
         time.sleep(5)
-        page_count = int(
-            driver.find_element(By.CSS_SELECTOR, "#SecView-page-count").text
+        return int(
+            self.__driver.find_element(By.CSS_SELECTOR, "#SecView-page-count").text
         )
 
-        image_name = "test_pages/{}.png"
-        page = 0
-        while page < page_count - 1:
-            #
-            # loadingEffect > div
-            # id = loadingEffect
-            #
-            try:
-                driver.find_element(By.ID, "loadingEffect")
-            except NoSuchElementException:
-                print(page)
-                with open(image_name.format(page), "wb") as file:
-                    file.write(
-                        driver.find_element(
-                            By.CSS_SELECTOR, "#SecViewPluginInner > div > img"
-                        ).screenshot_as_png
-                    )
-                driver.find_element(
-                    By.CSS_SELECTOR,
-                    "#SecViewPlugin > div.centerPart > div > div > div.controlBlock > div.navigationControlBlock > a.right",
-                ).click()
-                page += 1
-                continue
-        # exit webdriver
-        driver.close()
+    def __create_pdf(self, document_id: int):
+
+        dir_path = r'/test_pages/'
+
+        page_count = len(os.listdir(os.getcwd()+dir_path))
+
         # create pdf from images
+        #TODO: достать название документа и кол-во странициз БД
+        _, name = self.__get_doc(doc_id=document_id)
         images = [
-            Image.open(image_name.format(i)).convert("RGB") for i in range(page_count)
+            Image.open(self.image_name.format(i)).convert("RGB") for i in range(page_count)
         ]
 
-        pdf_path = os.getcwd() + f"/books/{document_id}.pdf"
+        pdf_path = os.getcwd() + f"/books/{name}.pdf"
 
         images[0].save(
             pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
         )
+        for path in os.listdir(os.getcwd()+dir_path):
+            p = os.path.join(os.getcwd()+dir_path, path)
+            os.remove(os.path.join(p))
+
+    def download_doc(self, document_id: int):
+
+        page_count = self.__get_page_count(document_id=document_id)
+        page = 0
+        bar = IncrementalBar('Pages downloaded', max=page_count)
+
+        while page < page_count - 1:
+            try:
+                self.__driver.find_element(By.ID, "loadingEffect")
+            except NoSuchElementException:
+                with open(self.image_name.format(page), "wb") as file:
+                    file.write(
+                        self.__driver.find_element(
+                            By.CSS_SELECTOR, "#SecViewPluginInner > div > img"
+                        ).screenshot_as_png
+                    )
+                self.__driver.find_element(
+                    By.CSS_SELECTOR,
+                    "#SecViewPlugin > div.centerPart > div > div > div.controlBlock > div.navigationControlBlock > a.right",
+                ).click()
+                page += 1
+                bar.next()
+                continue
+        bar.finish()
+        self.__create_pdf(document_id=document_id)
 
     def find_doc(self, doc_name: str):
         result = []
-        if self.db_session == None:
+        if self.db_session is None:
             self.db_session = Session()
-        docs = self.db_session.query(Document).all()
+        docs = self.db_session.query(models.Document).all()
         for i in docs:
             if doc_name in i.name:
                 result.append(i)
         return result
 
-    def get_folder(self, folder_id, s, db_session):
 
-        if folder_id == 0:
-
-            f = Folder(
-                id=0,
-                name="папки",
-                folder_id=0,
-            )
-            db_session.add(f)
-
-        r = s.get(self.folder_url.format(folder_id))
-        soup = bs4.BeautifulSoup(r.text, "html.parser")
-        table = soup.find_all("tbody")
-        rows = table[0].select("td.browse_column.title.sort_on")
-        for r in rows:
-            a = r.find_all("a")[0]
-            # определение типов в папке
-            if "fDocumentId" in a["href"]:
-                id_ = int(a["href"][75:])
-                name = a.getText()
-                print(f"Document: {id_}, {name}")
-                d = Document(
-                    id=id_,
-                    folder_id=folder_id,
-                    name=name,
-                )
-                db_session.add(d)
-                # self.get_doc(id_, name).get(s)
-                # print(f'doc with id: {a["href"][75:]}')
-            elif "fFolderId" in a["href"]:
-                id_ = int(a["href"][46:])
-                name = a.getText()
-                print(f"Folder: {id_}, {name}")
-                f = Folder(id=id_, name=name, folder_id=folder_id)
-                db_session.add(f)
-                self.get_folder(id_, s, db_session=db_session)
-                # print(f'folder with id: {a["href"][46:]}')
-            else:
-                print(f'uknown entity: {a["href"]}')
-
-    def update_lib(self):
-        # TODO: async
-        """
-        получение всех файлов в библиотеке
-        """
-        # TODO: получение всех зависимостей folder -> folder & folder -> file
-        _folder_id = 0
-        s = login()
-        r = s.get(self.folder_url.format(_folder_id))
-        if self.db_session != None:
-            self.db_session = Session()
-        self.get_folder(0, s)
-        self.db_session.commit()
-        self.db_session = None
-        logging.info(f"updated lib")
-
-
-# l = LibController()
-# id = l.find_doc("2100")
-# l.download_doc(id[0].id)
+l = LibController(debug=False)
+id = l.find_doc("Основы")
+print("".join([f"{i.id} {i.name}\n" for i in id]))
+l.download_doc(int(input()))
